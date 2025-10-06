149dc5cb0833eca9796c09972aa49bda3f0b6ccf
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-06 06:54:59 -0700

    add a js lect

---
+++ pro/index.js
@@ -0,0 +1,33 @@
+// variable
+// let , var ,const
+
+// let const
+
+var a="abc";
+console.log(`value of a ${a}`);
+// console.log("value of a ", a);
+
+var a=23;
+console.log(`value of a ${a}`);
+
+let B=5;
+console.log(`value of b ${B}`);
+let b="li";
+const con=2;
+console.log(con);
+console.log("message ",a);
+let sum=B + con;
+let dif=B - con;
+let dev=B / con;
+let mul=B * con;
+let nori=B % con;
+console.log(`the sum of two value ${sum}`);
+console.log(`the substract of two value ${dif}`);
+console.log(`the divesion of two value ${dev}`);
+console.log(`the multiply of two value ${mul}`);
+console.log(`the modulo of two value ${nori}`);
+
+
+
+
+
