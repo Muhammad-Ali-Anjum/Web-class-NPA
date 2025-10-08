fbd61aa99d96eb2cd5bc0445fe13f602d119e7d5
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-08 09:19:25 -0700

    add lect

---
+++ if.js
@@ -0,0 +1,22 @@
+mark=125;
+
+if (mark>80 && mark<100){
+    console.log("grade A++");
+    
+}
+else if(mark>100){
+    console.log("techncal error");
+
+}
+else if(mark>70 && mark<80){
+    console.log("grade A");
+
+}
+else if(mark>60 && mark<70){
+    console.log("grade B");
+
+}
+else{
+    console.log("fail");
+    
+}
