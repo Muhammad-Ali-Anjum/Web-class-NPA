88cc282704fc90409199ac09342d6b2378e6462d
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-10 06:26:00 -0700

    add lect

---
+++ first.js
@@ -0,0 +1,17 @@
+console.log("hellow world");
+// cout<<"";
+// print()
+// print()
+// FileS
+// let const var
+var a=32;
+var a=45;
+console.log(`value of a : ${a}`);
+let b=34;
+console.log(`value of b: ${b}`);
+
+// let b=454;
+const yawar=453;
+
+console.log(`value of constant: ${yawar}`);
+
