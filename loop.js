a1eaaee398139b394763694f3c53f90778882c7a
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-10 06:10:08 -0700

    add lect

---
+++ loop.js
@@ -0,0 +1,24 @@
+
+
+for(let i=0; i<10;i++){
+    if(i==3){
+        continue;
+    }
+    else{
+        console.log(i);
+        
+    }
+    
+}
+console.log("while loop");
+
+let i=6;
+while(i>0){
+console.log(i);
+i--;
+}
+
+do{
+    console.log(i);
+    i--;
+}(i>0);
