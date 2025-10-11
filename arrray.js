c6a5715bb9ad514cc7177bcb62669f8e348609e6
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-11 06:13:21 -0700

    add a sol file

---
+++ arrray.js
@@ -0,0 +1,9 @@
+// let a="nooori";
+// let b="muratza";
+// let c="farhan";
+
+// console.log(b);
+let web=["noori","murtaza","farhan"];
+console.log("\t",web[0]);
+console.log(web[0]);
+console.log(web[2]);
