363d3446cd5b16c6c9513f79d6543fbd8a98882f
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-17 07:14:35 -0700

    add an lect

---
+++ function.js
@@ -0,0 +1,62 @@
+
+function ShowName(){
+    console.log("murtaza");
+    
+}
+
+ShowName()
+
+
+function sum(num1,num2 ,num3){
+    console.log();
+    return num1+num2+num3
+
+    
+}
+// a=4
+// b=5
+// a=a+b
+    console.log(sum(2,3,3));
+    console.log(sum(2,4,3));
+
+function sum1(num1,num2 ,num3){
+  
+    return num1+num2+num3
+      console.log("this is a sum");
+
+    
+}
+
+
+sum1(1,3,4)
+
+
+let ab=[200,400,500]
+
+
+
+function check(...list){
+    console.log("list of ordr",list);
+    
+}
+check(200,550,333,90,23,54,545,5,454,45)
+
+
+function username(name , passwords){
+
+
+ if(!name || !passwords) {
+    console.log("plz enter name");
+    
+ }
+ else{
+ console.log(`user name ${name}`);
+ console.log(` password ${passwords}`);
+ }
+   
+    
+    
+    
+
+}
+username("noori", "reew")
