20ac5818ebdce1face7357577e0ade4b2a9a545f
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-10-23 06:37:03 -0700

    add a lect

--- Yawar/index.js
+++ Yawar/index.js
@@ -1,61 +1,82 @@
 
 
-let a=1;
-// while(a<5){
-//     console.log(a);
+// let a=1;
+// // while(a<5){
+// //     console.log(a);
+// //     a++;
+// // }
+// do{
+//      console.log(a);
 //     a++;
-// }
-do{
-     console.log(a);
-    a++;
 
-}while(a<10)
+// }while(a<10)
 
 
-    let i = 1;
-do {
-    // console.log(`i = ${i}`);
-    console.log("i=",i);
+//     let i = 1;
+// do {
+//     // console.log(`i = ${i}`);
+//     console.log("i=",i);
     
-    i++;
-} while (i <= 5);
-console.log("\n");
+//     i++;
+// } while (i <= 5);
+// console.log("\n");
 
 
-console.log("Example 2: Odd Numbers 1-19");
-let odd = 1;
-do {
-    console.log(odd);
-    odd += 2;
-} while (odd <= 19);
-console.log("\n");
+// console.log("Example 2: Odd Numbers 1-19");
+// let odd = 1;
+// do {
+//     console.log(odd);
+//     odd += 2;
+// } while (odd <= 19);
+// console.log("\n");
 
 
-console.log("Example 4: Reverse Triangle Pattern");
-let r = 5;
-do {
-    let line = '';
-    let c = 1;
-    do {
-        line += '* ';
-        c++;
-    } while (c <= r);
-    console.log(line);
-    r--;
-} while (r >= 1);
-console.log("\n");
+// console.log("Example 4: Reverse Triangle Pattern");
+// let r = 5;
+// do {
+//     let line = '';
+//     let c = 1;
+//     do {
+//         line += '* ';
+//         c++;
+//     } while (c <= r);
+//     console.log(line);
+//     r--;
+// } while (r >= 1);
+// console.log("\n");
 
-// Nested Multiplication Table 1x1 to 3x3
-console.log("Example 5: Nested Multiplication Table");
-let outer = 1;
-do {
-    let inner = 1;
-    do {
-        console.log(`${outer} x ${inner} = ${outer * inner}`);
-        inner++;
-    } while (inner <= 3);
-    outer++;
-} while (outer <= 3);
-console.log("\n");
+// // Nested Multiplication Table 1x1 to 3x3
+// console.log("Example 5: Nested Multiplication Table");
+// let outer = 1;
+// do {
+//     let inner = 1;
+//     do {
+//         console.log(`${outer} x ${inner} = ${outer * inner}`);
+//         inner++;
+//     } while (inner <= 3);
+//     outer++;
+// } while (outer <= 3);
+// console.log("\n");
 
-console.log("✅ DO-WHILE Loop Lecture End");
+// console.log("✅ DO-WHILE Loop Lecture End");
+
+
+let arr=[1,2,4,5,7,8,9];
+// console.log(arr);
+// console.log(arr[4]);
+// console.log(arr[0]);
+// console.log(arr.push(32));
+// console.log(arr.pop());
+// console.log(arr.pop());
+// console.log(arr.pop());
+// console.log(arr);
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+    
+// }
+
+console.log(arr.reverse());
+
+
+
+
