6f492d175e693e5a08f37047516cbb3d9b147576
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-11-08 18:42:18 +0500

    add a lect

--- Lect/js/index.js
+++ Lect/js/index.js
@@ -1,1 +1,47 @@
 console.log("hi programer");
+
+
+// js data type
+// let 
+// var
+// const
+var a=30;
+// console.log(a);
+var a=45;
+console.log(a);
+// es6
+
+let b=34;
+console.log(b);
+
+
+let v="ali";
+console.log(v);
+const add=34;
+console.log(add);
+
+// oprators 
+// = asgn
+// == equal oprators
+// === equal typecosting
+// < less then
+// > grater then
+// <=>=
+
+// logical gate
+
+// && and opr
+
+// T T => T
+// T F => F
+// F T =>F
+
+// || or opr
+// T T T
+// T F T
+// F T T
+// F F F
+
+// ! not opr
+// T F 
+// F T
