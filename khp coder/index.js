676d022d4bbfc5241fbedc7e9f5b7f9c11bc049e
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-12-11 16:28:20 +0500

    add a file

--- khp coder/index.js
+++ khp coder/index.js
@@ -1,5 +1,68 @@
-console.log("hi javascript ");
-console.log("in programming ");
-console.log("Khp coder  ");
-let a=23;
+// let a =43;
+// console.log("This is value of a",a);
+// console.log(`this is a value of : ${a}`);
+
+// data type
+
+// let 
+// var
+// const
+var a=24;
+
+var a=89;
 console.log(a);
+
+
+// const a=32;
+
+
+// operter
+// arth opr
+// let c= c+54;
+let c =54;
+c=c+43;
+console.log(c);
+c+=43;
+console.log(c);
+
+// log
+
+// && and opener
+// T T T
+// T F F
+// F T F
+// F F F
+
+// or oper
+
+// T T T 
+// T F T
+// F T T
+// F F F
+let num=434
+
+if(num>30 && num<40 ){
+    console.log("eligble");
+    
+}
+else{
+    console.log("not equall");
+    
+}
+let pass="npa";
+if (pass==="npa"){
+    console.log("login");
+    
+}
+else{
+    console.log("password not correct");
+
+
+}
+if(pass!="npa"){
+    console.log("password not correct");
+    
+}
+
+
+
