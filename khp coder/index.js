c1a87da67a1fdf356645c2366c4a042dd3704e09
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-12-14 15:33:48 +0500

    add a lect

--- khp coder/index.js
+++ khp coder/index.js
@@ -65,45 +65,68 @@
 // }
 
 
-const per=60;
+// const per=60;
 
-if(per>=80 && per<=100 ){
-    console.log("Grade A+");
+// if(per>=80 && per<=100 ){
+//     console.log("Grade A+");
     
-}
-else if(per=>60 && per<80){
-    console.log("Grate A");
+// }
+// else if(per=>60 && per<80){
+//     console.log("Grate A");
     
-}
-else if(per>100){
-console.log("tech error ");
+// }
+// else if(per>100){
+// console.log("tech error ");
 
-}
-else{
-    console.log("Fail");
+// }
+// else{
+//     console.log("Fail");
     
-}
-// this 
-console.log(1+true);
-console.log(5*false);
+// }
+// // this 
+// console.log(1+true);
+// console.log(5*false);
+// // true 1
+// // false 0
 
-// true 1
-// false 0
+// const opr="-";
 
-const opr="-";
+// switch(opr){
+//     case "+":
+//         console.log(" add :",4+5);
+//         break
+//     case "-":
+//         console.log("sub: ",43-34);
+//         break;
+//     default:
+//         console.log("something wrong");
+// }
 
-switch(opr){
-    case "+":
-        console.log(" add :",4+5);
-        break
-    case "-":
-        console.log("sub: ",43-34);
-        break;
-    default:
-        console.log("something wrong");
-        
-        
-        
+// loop
+// for, while ,do while, for each,Mapp reduce ,
+// let a =1
+// let b =2
+// let c=3
 
-}
+// for(let i=1;i<10;i++){ 
+//    if (i== 3 || i==6){
+//     continue
+//    }
+//    else{
+// console.log(i);
 
+//    }   
+// }
+let t=5
+// while(t<10){
+//     t++;
+//     console.log(t); 
+// }
+console.log("do while loop");
+
+do{
+      
+    console.log(t);
+     t++
+ 
+}while(t<10)
