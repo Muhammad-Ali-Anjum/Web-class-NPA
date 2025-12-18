a004a14d6e6fb6e4e0607fe2442434036d98634e
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-12-18 15:10:15 +0500

    add a lect

---
+++ khp coder/object.js
@@ -0,0 +1,14 @@
+// object
+let person={
+    name:"Inam",
+    PhNo:232,
+    id:7230324235,
+    address:{
+        city:"Skardu",
+        zip:16100,
+        steet:"abc123 skardu",
+
+
+    }
+}
+console.log(person.address);
