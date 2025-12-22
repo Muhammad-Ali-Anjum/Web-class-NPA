60adbff14cc9f224ebd46b779949600b76d5306d
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-12-22 15:48:02 +0500

    add a lect

---
+++ Lect/Function/arrow.function.js
@@ -0,0 +1,15 @@
+let fun=()=>{
+    console.log("The erow function is runing");
+    
+}
+fun()
+
+let single =()=> console.log("the function  is runing");
+single()
+
+let sum=(a,b)=> a+b;
+console.log(sum(2,3));
+let sums=(a,b)=>{
+return a+b
+} ;
+console.log(sums(3,2));
