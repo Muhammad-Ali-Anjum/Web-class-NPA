316a05a35a13c2d9dc8dab4d019ce7f647ade10a
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2025-12-23 15:20:36 +0500

    add a lect

--- Lect/fun.js
+++ Lect/fun.js
@@ -1,9 +1,27 @@
-import React from 'react'
 
-function fun() {
-  return (
-    <div>function</div>
-  )
+function sum(){
+console.log("the fuction is running");
+
 }
 
-export default fun
+sum()
+
+
+function sum(a,s)
+{
+    return a+s;
+}
+let sm=sum(23,4)
+console.log(sm);
+function sum(a,s,d){
+    return a+s+d;
+}
+let sums=sum(2,2,4)
+console.log(sums);
+// function smd(a,...s){
+//     return a - s.log(smd(2,65));
+function inf(...a){
+return a+= a;
+}
+console.log(`the sum of two number: ${inf(2,4,4,7,8,99,3)}`);
+
