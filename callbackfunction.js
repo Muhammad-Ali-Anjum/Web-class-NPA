d4a7119e471a5d908de98b3e805db74856b7d6c4
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2026-01-17 16:28:10 +0500

    add a lect

--- callbackfunction.js
+++ callbackfunction.js
@@ -1,4 +1,38 @@
-setTimeout(function IsApiRin(){
-console.log("api Runing ");
+// setTimeout(function IsApiRin(){
+// console.log("api Runing ");
 
-},1000)
+// },1000)
+
+
+console.log("start");
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log("Sum is:", sum);
+
+let sub = b -a;
+console.log("Subtraction is:", sub);
+
+if (sub > sum) {
+    console.log("Subtraction is greater than Sum");
+} else {
+    console.log("Sum is greater than Subtraction");
+}
+
+let grade = 85;
+
+if (grade >= 50) {
+    console.log("A Grade");
+}else{
+    console.log("fail");
+    
+}
+
+let age = 12;
+
+if (age < 18) {
+    console.log("not eligible to vote");
+}
+else {
+    console.log("eligible to vote");
+}
