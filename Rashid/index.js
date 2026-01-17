5e9c3e13e18641a4931cb48684e326a193d9117e
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2026-01-17 16:40:41 +0500

    add a lect

---
+++ Rashid/index.js
@@ -0,0 +1,25 @@
+// logal operter
+// && oper
+// T T T
+// T F F
+// F T F
+// F F F
+// OR || OPR
+// T T T
+// T F T
+// F T T
+// F F F
+// NOT !
+// T F
+// F T
+console.log("setart");
+
+let mark=45;
+if(mark>30 && mark<=100){
+    console.log("pass");
+    
+}
+else{
+    console.log("fail");
+    
+}
