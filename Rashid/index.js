4ad16ef34d351802f2669b7f632ea5564a043694
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2026-01-18 16:18:09 +0500

    add card filee

--- Rashid/index.js
+++ Rashid/index.js
@@ -13,13 +13,30 @@
 // T F
 // F T
 console.log("setart");
+const total=85+90+80;
+const mark=(total/300)*100;
+console.log("percentage",mark);
 
-let mark=45;
-if(mark>30 && mark<=100){
-    console.log("pass");
+// let mark=45;
+if(mark>80 && mark<=100){
+    console.log("A");
     
 }
-else{
-    console.log("fail");
-    
-}
+else if(mark>=60 && mark<80){
+    console.log("B"); 
+  }
+else if(mark>=40 && mark<60){
+    console.log("C");  
+ }
+else if(mark>=30 && mark<40){
+    console.log("D");  
+ }
+
+else if(mark<0 || mark>100){
+    console.log("invalid");
+  }
+ else{
+        console.log("Fail");
+        
+    }
+
