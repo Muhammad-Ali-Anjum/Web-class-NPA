9cf1e37112b16539043903a3d4eff41be18136e9
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2026-01-22 15:19:26 +0500

    add a transfrom

--- Rashid/index.js
+++ Rashid/index.js
@@ -1,64 +1,80 @@
-// logal operter
-// && oper
-// T T T
-// T F F
-// F T F
-// F F F
-// OR || OPR
-// T T T
-// T F T
-// F T T
-// F F F
-// NOT !
-// T F
-// F T
-console.log("setart");
-const total=85+90+80;
-const mark=(total/300)*100;
-console.log("percentage",mark);
+// // logal operter
+// // && oper
+// // T T T
+// // T F F
+// // F T F
+// // F F F
+// // OR || OPR
+// // T T T
+// // T F T
+// // F T T
+// // F F F
+// // NOT !
+// // T F
+// // F T
+// console.log("setart");
+// const total=85+90+80;
+// const mark=(total/300)*100;
+// console.log("percentage",mark);
 
-// let mark=45;
-if(mark>80 && mark<=100){
-    console.log("A");
+// // let mark=45;
+// if(mark>80 && mark<=100){
+//     console.log("A");
     
-}
-else if(mark>=60 && mark<80){
-    console.log("B"); 
-  }
-else if(mark>=40 && mark<60){
-    console.log("C");  
- }
-else if(mark>=30 && mark<40){
-    console.log("D");  
- }
+// }
+// else if(mark>=60 && mark<80){
+//     console.log("B"); 
+//   }
+// else if(mark>=40 && mark<60){
+//     console.log("C");  
+//  }
+// else if(mark>=30 && mark<40){
+//     console.log("D");  
+//  }
 
-else if(mark<0 || mark>100){
-    console.log("invalid");
-  }
- else{
-        console.log("Fail");        
-    }
+// else if(mark<0 || mark>100){
+//     console.log("invalid");
+//   }
+//  else{
+//         console.log("Fail");        
+//     }
 
 
 
-console.log("========================class start===========================");
+// console.log("========================class start===========================");
 
-let opr="+";
+// let opr="+";
 
-switch(opr){
-    case "+":
-        let a=5;
-        let b=10;
+// switch(opr){
+//     case "+":
+//         let a=5;
+//         let b=10;
 
-        console.log("Addition", a+b);   
-        break;
-    case "-": 
-        console.log("Subtraction");
-        break;
-    case "*":
-        console.log("Multiplication"); 
-        break;
-    default:
-        console.log("invlid operator");
-}
+//         console.log("Addition", a+b);   
+//         break;
+//     case "-": 
+//         console.log("Subtraction");
+//         break;
+//     case "*":
+//         console.log("Multiplication"); 
+//         break;
+//     default:
+//         console.log("invlid operator");
+// }
         
+
+// for(let i=1;i<=;i++){
+    // let row=""; 
+    // for(let j=1;j<=5;j++){
+    //     row=row+"* ";
+    //     console.log(row);
+    // }   
+    
+    
+// }
+
+for(let i=5;i>=1;i--){
+    for(let j=1;j<=i;j++){
+        console.log(j);
+    }
+}
