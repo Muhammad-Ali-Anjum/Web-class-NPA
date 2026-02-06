1cdd77a520c535725bd835e23430dea8a7289f50
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-06 15:20:16 +0500

    add a js rest operator

---
+++ sara coder/Bootstrap/js/first.js
@@ -0,0 +1,6 @@
+// app.js
+const fs = require('fs');
+
+// Create a new file and write text into it
+fs.writeFileSync('example.txt', 'Hello from Node.js!');
+console.log('File created successfully!');
