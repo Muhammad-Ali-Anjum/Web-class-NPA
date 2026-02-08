bcdc5eafbcefd556968513dfdf72a9b962a20705
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-08 16:31:26 +0500

    add a js rest operator

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -9,6 +9,7 @@ var a=43;
 var user="admin"
 
 var user="user"
+var user="e"
 console.log(user);
 
 var a=12;
@@ -17,11 +18,12 @@ const b=32;
 // const b=54;
 console.log(b);
 let users="admin"
+// let users="34"
 console.log(users);
 // let users="user";
 console.log(users);
 
-console.log(true+1);
+console.log(true+2);
 console.log(false*10);
 
 // operator
@@ -59,6 +61,60 @@ console.log(false*10);
 // /
 // %
 
+// if(condaction){
+//     block of code 
+// }
 
+let user1="Ali";
+if(user1=="Ali"){
+    console.log("login sucessfull"); 
+}
+let ali="ali";
+let Ali="ali";
+if(user1=="ali"){
+    console.log("login sucessfull"); 
 
-// 
+}
+else{
+    console.log("login fail");
+    
+}
+console.log("======================================loign from=====================");
+
+let pswd=1234
+if(user1=="Ali" && pswd==1234){
+    console.log("login sucessfull"); 
+    
+}
+else{
+    console.log("login fail");
+    
+}
+
+let st="Death";
+let age=18;
+if(st=="Active" && age>=18){
+console.log("cast vote");
+
+}else{
+    console.log("not eligible");
+    
+}
+let obt=80+80+80+80+90+80;
+let total=600;
+console.log(obt);
+
+let mark=(obt/total)*100;
+
+// let mark=180;
+if(mark>=80 && mark<=100 ){
+    console.log("Grade A");
+    
+}
+else if(mark>100){
+console.log("technical issue check the insert subject %");
+
+}
+else{
+    log("Grade F")
+}
