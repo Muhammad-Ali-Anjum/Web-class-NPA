d9ea8f20b323453bc234ac47dd6e6a219f512b8e
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-09 23:36:18 +0500

    add a lect

--- arrray.js
+++ arrray.js
@@ -1,26 +1,26 @@
-// let a="nooori";
-// let b="muratza";
-// let c="farhan";
+let a="nooori";
+let b="muratza";
+let c="farhan";
 
-// console.log(b);
-// let web=["noori","murtaza","farhan"];
-// console.log(web[0]);
-// console.log(web[0]);
-// console.log(web[2]);
-// web.push("Ali");
-// web.push("anjum")
-// console.log(web);
-// web.pop()
-// web.pop()
-// web.pop()
-// web.pop()
-// web.pop()
-// web.pop("noori")
-// console.log(web);
-// console.log(web.length);
-// console.log(web.reverse());
-// console.log(`shifted walue is ${web.shift()}`);
-// console.log(web.slice(1,2));
+console.log(b);
+let web=["noori","murtaza","farhan"];
+console.log(web[0]);
+console.log(web[0]);
+console.log(web[2]);
+web.push("Ali");
+web.push("anjum")
+console.log(web);
+web.pop();
+web.pop();
+web.pop();
+web.pop();
+web.pop();
+web.pop("noori")
+console.log(web);
+console.log(web.length);
+console.log(web.reverse());
+console.log(`shifted walue is ${web.shift()}`);
+console.log(web.slice(1,2));
 
 
 
