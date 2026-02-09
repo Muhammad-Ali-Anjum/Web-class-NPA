33ce7fd1416f9db5fd7e5228dd3189be1b277972
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-09 23:32:21 +0500

    add a lect

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -1,120 +1,137 @@
-// console.log("this is a first program");
-// console.log("runing..........");
-// console.log("nodmon is working");
+// // console.log("this is a first program");
+// // console.log("runing..........");
+// // console.log("nodmon is working");
 
-// console.log("class end");
+// // console.log("class end");
 
 
-var a=43;
-var user="admin"
+// var a=43;
+// var user="admin"
 
-var user="user"
-var user="e"
-console.log(user);
+// var user="user"
+// var user="e"
+// console.log(user);
 
-var a=12;
-console.log(a);
-const b=32;
-// const b=54;
-console.log(b);
-let users="admin"
-// let users="34"
-console.log(users);
-// let users="user";
-console.log(users);
+// var a=12;
+// console.log(a);
+// const b=32;
+// // const b=54;
+// console.log(b);
+// let users="admin"
+// // let users="34"
+// console.log(users);
+// // let users="user";
+// console.log(users);
 
-console.log(true+2);
-console.log(false*10);
+// console.log(true+2);
+// console.log(false*10);
 
-// operator
-// = asssign ope
-// <> 
-// ! not equal
-// comp
-// == eq
-// === eq to 
-// !=
-// <= >=
+// // operator
+// // = asssign ope
+// // <> 
+// // ! not equal
+// // comp
+// // == eq
+// // === eq to 
+// // !=
+// // <= >=
 
-// logcal op
+// // logcal op
 
-// and && oper
-// T T T
-// T F F
-// F T F
-// F F F
+// // and && oper
+// // T T T
+// // T F F
+// // F T F
+// // F F F
 
-// OR OPR ||
-// T T T 
-// T F T
-// F T T
-// F F F
+// // OR OPR ||
+// // T T T 
+// // T F T
+// // F T T
+// // F F F
 
-// NOT OP !
-// T F 
-// F T
+// // NOT OP !
+// // T F 
+// // F T
 
-// Arthamatic
-// +
-// -
-// *
-// /
-// %
+// // Arthamatic
+// // +
+// // -
+// // *
+// // /
+// // %
 
-// if(condaction){
-//     block of code 
+// // if(condaction){
+// //     block of code 
+// // }
+
+// let user1="Ali";
+// if(user1=="Ali"){
+//     console.log("login sucessfull"); 
+// }
+// let ali="ali";
+// let Ali="ali";
+// if(user1=="ali"){
+//     console.log("login sucessfull"); 
+
+// }
+// else{
+//     console.log("login fail");
+    
+// }
+// console.log("======================================loign from=====================");
+
+// let pswd=1234
+// if(user1=="Ali" && pswd==1234){
+//     console.log("login sucessfull"); 
+    
+// }
+// else{
+//     console.log("login fail");
+    
 // }
 
-let user1="Ali";
-if(user1=="Ali"){
-    console.log("login sucessfull"); 
-}
-let ali="ali";
-let Ali="ali";
-if(user1=="ali"){
-    console.log("login sucessfull"); 
+// let st="Death";
+// let age=18;
+// if(st=="Active" && age>=18){
+// console.log("cast vote");
 
-}
-else{
-    console.log("login fail");
+// }else{
+//     console.log("not eligible");
     
-}
-console.log("======================================loign from=====================");
+// }
+// let obt=80+80+80+80+90+80;
+// let total=600;
+// console.log(obt);
 
-let pswd=1234
-if(user1=="Ali" && pswd==1234){
-    console.log("login sucessfull"); 
+// let mark=(obt/total)*100;
+
+// // let mark=180;
+// if(mark>=80 && mark<=100 ){
+//     console.log("Grade A");
     
-}
-else{
-    console.log("login fail");
+// }
+// else if(mark>100){
+// console.log("technical issue check the insert subject %");
+
+// }
+// else{
+//     log("Grade F")
+// }
+
+let oper="%";
+switch(oper){
+    case "-":
+
+        console.log("sub");
+        break;
+    case "+":
+        console.log("add");
+        break;
+    case "*":
+        console.log("muilti");
+        break;
+    default:
+        console.log("not vilid");
     
-}
-
-let st="Death";
-let age=18;
-if(st=="Active" && age>=18){
-console.log("cast vote");
-
-}else{
-    console.log("not eligible");
-    
-}
-let obt=80+80+80+80+90+80;
-let total=600;
-console.log(obt);
-
-let mark=(obt/total)*100;
-
-// let mark=180;
-if(mark>=80 && mark<=100 ){
-    console.log("Grade A");
-    
-}
-else if(mark>100){
-console.log("technical issue check the insert subject %");
-
-}
-else{
-    log("Grade F")
 }
