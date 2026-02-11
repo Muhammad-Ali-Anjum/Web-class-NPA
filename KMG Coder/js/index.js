e7d0abde784293fda5254e1fbddf196053bcc592
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-11 15:32:40 +0500

    add a basic setup of teilwind css

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -119,19 +119,94 @@
 //     log("Grade F")
 // }
 
-let oper="%";
-switch(oper){
-    case "-":
+// let oper="%";
+// switch(oper){
+//     case "-":
 
-        console.log("sub");
-        break;
-    case "+":
-        console.log("add");
-        break;
-    case "*":
-        console.log("muilti");
-        break;
-    default:
-        console.log("not vilid");
+//         console.log("sub");
+//         break;
+//     case "+":
+//         console.log("add");
+//         break;
+//     case "*":
+//         console.log("muilti");
+//         break;
+//     default:
+//         console.log("not vilid");
     
-}
+// }
+
+// loop
+// types 
+// for loop
+// while loop
+// do while loop
+
+// for(initilization,condation,increament/dec){
+//     b code
+// }
+// for (let i=10;i>0;i--){
+//     console.log(i);
+    
+// }
+// let a=2
+// for(let i=1;i<=20;i++){
+//     // console.log(a," x ",i ,"= " ,a*i);
+//     console.log(`${a} X ${i} = ${a*i}`);
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i%2==0){
+//         console.log(i);
+        
+//     }
+    
+// }
+// console.log("break sataem");
+
+// for(let i=1;i<=10;i++){
+// if(i==3){
+//     break
+// }
+// console.log(i);
+
+// }
+
+// console.log("conti sataem");
+
+// for(let i=1;i<=10;i++){
+// if(i==3 || i==4 || i==6){
+//     continue;
+// }
+// console.log(i);
+
+// }
+// console.log("+++++++++++++++++++");
+
+// for(let i=1;i<=10;i++){
+//     if(i%2==0){
+       
+//         continue
+//     }
+//      console.log(i);
+    
+// }
+console.log("++++++++++++++++");
+
+let a=11;
+// while (a<10){
+
+//     if(a%2===0){
+//        console.log(a);
+//     }
+// a++
+
+// }
+
+do{
+    a++
+ if(a%2!=0){
+       console.log(a);
+    }
+
+}while(a<10)
