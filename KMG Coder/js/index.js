9aa060264782a5c065686889b1ea43d3f29c3ba1
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-12 17:29:06 +0500

    add an lect

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -191,22 +191,38 @@
 //      console.log(i);
     
 // }
-console.log("++++++++++++++++");
+// 
 
-let a=11;
-// while (a<10){
-
-//     if(a%2===0){
-//        console.log(a);
+// for(let i=1;i<=10;i++){
+//     if(i==3 || i==4 || i==6){
+//         continue;
 //     }
-// a++
 
+//     console.log(i);
 // }
 
-do{
-    a++
- if(a%2!=0){
-       console.log(a);
-    }
+for(let i=1;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(`${i} X ${j} = ${i*j}`);
+    }  }
 
-}while(a<10)
+// for(let i=1;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log("*" ,"");}}
+        
+for(let i=1;i<=5;i++){ 
+    let str="";
+    for(let j=1;j<=5;j++){
+        str+="* ";
+    }
+    console.log(str);
+}
+
+
+for(i=0;i<5;i++){
+    let str=""; 
+    for(j=0;j<=i;j++){
+        str+="* ";
+    }
+    console.log(str);}
+
