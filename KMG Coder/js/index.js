84ad768a35c6fc71f26cddb13d11641e22071853
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-15 17:00:03 +0500

    add a lect

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -1,228 +1,48 @@
-// // console.log("this is a first program");
-// // console.log("runing..........");
-// // console.log("nodmon is working");
+// // 
 
-// // console.log("class end");
-
-
-// var a=43;
-// var user="admin"
-
-// var user="user"
-// var user="e"
-// console.log(user);
-
-// var a=12;
-// console.log(a);
-// const b=32;
-// // const b=54;
-// console.log(b);
-// let users="admin"
-// // let users="34"
-// console.log(users);
-// // let users="user";
-// console.log(users);
-
-// console.log(true+2);
-// console.log(false*10);
-
-// // operator
-// // = asssign ope
-// // <> 
-// // ! not equal
-// // comp
-// // == eq
-// // === eq to 
-// // !=
-// // <= >=
-
-// // logcal op
-
-// // and && oper
-// // T T T
-// // T F F
-// // F T F
-// // F F F
-
-// // OR OPR ||
-// // T T T 
-// // T F T
-// // F T T
-// // F F F
-
-// // NOT OP !
-// // T F 
-// // F T
-
-// // Arthamatic
-// // +
-// // -
-// // *
-// // /
-// // %
-
-// // if(condaction){
-// //     block of code 
-// // }
-
-// let user1="Ali";
-// if(user1=="Ali"){
-//     console.log("login sucessfull"); 
-// }
-// let ali="ali";
-// let Ali="ali";
-// if(user1=="ali"){
-//     console.log("login sucessfull"); 
+// // function
+// function name(){
+// console.log("server is runing....");
 
 // }
-// else{
-//     console.log("login fail");
+
+// name();
+
+// function add(a,b){
+//     console.log(a+b);
     
 // }
-// console.log("======================================loign from=====================");
+// add(10,30);
 
-// let pswd=1234
-// if(user1=="Ali" && pswd==1234){
-//     console.log("login sucessfull"); 
-    
+// function sub(a,b){
+// return a-b;
 // }
-// else{
-//     console.log("login fail");
-    
+// let result=sub(20,10);
+// console.log(result);
+
+
+// arrow function
+// let add=(a,b)=>{
+//     console.log(a+b);
 // }
-
-// let st="Death";
-// let age=18;
-// if(st=="Active" && age>=18){
-// console.log("cast vote");
-
-// }else{
-//     console.log("not eligible");
-    
-// }
-// let obt=80+80+80+80+90+80;
-// let total=600;
-// console.log(obt);
-
-// let mark=(obt/total)*100;
-
-// // let mark=180;
-// if(mark>=80 && mark<=100 ){
-//     console.log("Grade A");
-    
-// }
-// else if(mark>100){
-// console.log("technical issue check the insert subject %");
-
-// }
-// else{
-//     log("Grade F")
-// }
-
-// let oper="%";
-// switch(oper){
-//     case "-":
-
-//         console.log("sub");
-//         break;
-//     case "+":
-//         console.log("add");
-//         break;
-//     case "*":
-//         console.log("muilti");
-//         break;
-//     default:
-//         console.log("not vilid");
-    
-// }
-
-// loop
-// types 
-// for loop
-// while loop
-// do while loop
-
-// for(initilization,condation,increament/dec){
-//     b code
-// }
-// for (let i=10;i>0;i--){
-//     console.log(i);
-    
-// }
-// let a=2
-// for(let i=1;i<=20;i++){
-//     // console.log(a," x ",i ,"= " ,a*i);
-//     console.log(`${a} X ${i} = ${a*i}`);
-// }
-
-// for(let i=1;i<=10;i++){
-//     if(i%2==0){
-//         console.log(i);
-        
-//     }
-    
-// }
-// console.log("break sataem");
-
-// for(let i=1;i<=10;i++){
-// if(i==3){
-//     break
-// }
-// console.log(i);
-
-// }
-
-// console.log("conti sataem");
-
-// for(let i=1;i<=10;i++){
-// if(i==3 || i==4 || i==6){
-//     continue;
-// }
-// console.log(i);
-
-// }
-// console.log("+++++++++++++++++++");
-
-// for(let i=1;i<=10;i++){
-//     if(i%2==0){
-       
-//         continue
-//     }
-//      console.log(i);
-    
-// }
-// 
-
-// for(let i=1;i<=10;i++){
-//     if(i==3 || i==4 || i==6){
-//         continue;
-//     }
-
-//     console.log(i);
-// }
-
-for(let i=1;i<=10;i++){
-    for(let j=1;j<=10;j++){
-        console.log(`${i} X ${j} = ${i*j}`);
-    }  }
-
-// for(let i=1;i<=10;i++){
-//     for(let j=1;j<=10;j++){
-//         console.log("*" ,"");}}
-        
-for(let i=1;i<=5;i++){ 
-    let str="";
-    for(let j=1;j<=5;j++){
-        str+="* ";
-    }
-    console.log(str);
+// add(10,20);
+let hi=()=>{
+    console.log("hi");
 }
+hi();
+let syed=()=>{
+    console.log("syed");
+}
+syed();
+let syeed=()=> console.log("syeed");
 
-
-for(i=0;i<5;i++){
-    let str=""; 
-    for(j=0;j<=i;j++){
-        str+="* ";
-    }
-    console.log(str);}
-
+syeed();
+let add=(a,b)=> {
+    return a+b;
+}
+let result=add(10,20);
+console.log(result);
+console.log("==================");
+let adds=(a,b)=> a+b;
+let results=adds(20,30);
+console.log(results);
