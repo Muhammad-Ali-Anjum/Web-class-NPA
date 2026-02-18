4a8ef3d08647a438023a3e308e8a013eb6362c0e
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-18 16:00:28 +0500

    add new lect

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -1,48 +1,111 @@
-// // 
+// // // 
 
-// // function
-// function name(){
-// console.log("server is runing....");
+// // // function
+// // function name(){
+// // console.log("server is runing....");
 
-// }
+// // }
 
-// name();
+// // name();
 
-// function add(a,b){
-//     console.log(a+b);
+// // function add(a,b){
+// //     console.log(a+b);
     
-// }
-// add(10,30);
+// // }
+// // add(10,30);
 
-// function sub(a,b){
-// return a-b;
-// }
-// let result=sub(20,10);
-// console.log(result);
+// // function sub(a,b){
+// // return a-b;
+// // }
+// // let result=sub(20,10);
+// // console.log(result);
 
 
-// arrow function
-// let add=(a,b)=>{
-//     console.log(a+b);
-// }
-// add(10,20);
-let hi=()=>{
-    console.log("hi");
+// // arrow function
+// // let add=(a,b)=>{
+// //     console.log(a+b);
+// // }
+// // add(10,20);
+// // let hi=()=>{
+// //     console.log("hi");
+// // }
+// // hi();
+// // let syed=()=>{
+// //     console.log("syed");
+// // }
+// // syed();
+// // let syeed=()=> console.log("syeed");
+
+// // syeed();
+// // let add=(a,b)=> {
+// //     return a+b;
+// // }
+// // let result=add(10,20);
+// // console.log(result);
+// // console.log("==================");
+// // let adds=(a,b)=> a+b;
+// // let results=adds(20,30);
+// // console.log(results);
+
+// // const checkAge = (age) => age >= 18 ? 'Adult' : 'Minor';
+
+// // console.log(checkAge(20)); 
+// // console.log(checkAge(15));
+
+// // let votechecker=(age)=>{
+// //     return age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // }
+// // console.log(votechecker(20));
+// // console.log(votechecker(15));
+
+// // let votecheck=(age)=> age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // console.log(votecheck(20));
+// // console.log(votecheck(15));
+// // let votecheck=age=> age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // console.log(votecheck(20));
+// // console.log(votecheck(15));
+
+// let votechecks=age=> {
+//     if(age>=18){
+//         return "you are eligible for vote";
+//     }
+//     return "you are not eligible for vote";
+// };
+// // console.log(votechecks(20));
+// console.log(votechecks(15));
+
+
+// Advanced Techniques with Higher Order Functions
+function add(x) {
+    return x + 2;
 }
-hi();
-let syed=()=>{
-    console.log("syed");
+function mul(x) {
+    return x * 3;
 }
-syed();
-let syeed=()=> console.log("syeed");
+function compose(f, g) {
+    return function(x) {
+        return f(g(x));
+  }; }
+const addThenMul = compose(mul, add)(4);
+// const result = addThenMul(4);
+console.log(addThenMul); // Output: 21
 
-syeed();
-let add=(a,b)=> {
-    return a+b;
+function mul(x) {
+    return function(y) {
+        return x * y;
+  };
 }
-let result=add(10,20);
-console.log(result);
-console.log("==================");
-let adds=(a,b)=> a+b;
-let results=adds(20,30);
-console.log(results);
+var mulFn = mul(2);
+console.log(mulFn(5));
+
+function hi(kuchbifun){
+    console.log("first function");
+    kuchbifun()
+    
+}
+function kuchbifun(){
+    console.log("second function");
+}
+
+
+hi(kuchbifun);
--- Rashid/index.js
+++ Rashid/index.js
@@ -1,224 +1,265 @@
-// // // logal operter
-// // // && oper
-// // // T T T
-// // // T F F
-// // // F T F
-// // // F F F
-// // // OR || OPR
-// // // T T T
-// // // T F T
-// // // F T T
-// // // F F F
-// // // NOT !
-// // // T F
-// // // F T
-// // console.log("setart");
-// // const total=85+90+80;
-// // const mark=(total/300)*100;
-// // console.log("percentage",mark);
+// // // // logal operter
+// // // // && oper
+// // // // T T T
+// // // // T F F
+// // // // F T F
+// // // // F F F
+// // // // OR || OPR
+// // // // T T T
+// // // // T F T
+// // // // F T T
+// // // // F F F
+// // // // NOT !
+// // // // T F
+// // // // F T
+// // // console.log("setart");
+// // // const total=85+90+80;
+// // // const mark=(total/300)*100;
+// // // console.log("percentage",mark);
 
-// // // let mark=45;
-// // if(mark>80 && mark<=100){
-// //     console.log("A");
+// // // // let mark=45;
+// // // if(mark>80 && mark<=100){
+// // //     console.log("A");
     
-// // }
-// // else if(mark>=60 && mark<80){
-// //     console.log("B"); 
-// //   }
-// // else if(mark>=40 && mark<60){
-// //     console.log("C");  
-// //  }
-// // else if(mark>=30 && mark<40){
-// //     console.log("D");  
-// //  }
+// // // }
+// // // else if(mark>=60 && mark<80){
+// // //     console.log("B"); 
+// // //   }
+// // // else if(mark>=40 && mark<60){
+// // //     console.log("C");  
+// // //  }
+// // // else if(mark>=30 && mark<40){
+// // //     console.log("D");  
+// // //  }
 
-// // else if(mark<0 || mark>100){
-// //     console.log("invalid");
-// //   }
-// //  else{
-// //         console.log("Fail");        
-// //     }
+// // // else if(mark<0 || mark>100){
+// // //     console.log("invalid");
+// // //   }
+// // //  else{
+// // //         console.log("Fail");        
+// // //     }
 
 
 
-// // console.log("========================class start===========================");
+// // // console.log("========================class start===========================");
 
-// // let opr="+";
+// // // let opr="+";
 
-// // switch(opr){
-// //     case "+":
-// //         let a=5;
-// //         let b=10;
+// // // switch(opr){
+// // //     case "+":
+// // //         let a=5;
+// // //         let b=10;
 
-// //         console.log("Addition", a+b);   
-// //         break;
-// //     case "-": 
-// //         console.log("Subtraction");
-// //         break;
-// //     case "*":
-// //         console.log("Multiplication"); 
-// //         break;
-// //     default:
-// //         console.log("invlid operator");
-// // }
+// // //         console.log("Addition", a+b);   
+// // //         break;
+// // //     case "-": 
+// // //         console.log("Subtraction");
+// // //         break;
+// // //     case "*":
+// // //         console.log("Multiplication"); 
+// // //         break;
+// // //     default:
+// // //         console.log("invlid operator");
+// // // }
         
 
-// // for(let i=1;i<=;i++){
-//     // let row=""; 
-//     // for(let j=1;j<=5;j++){
-//     //     row=row+"* ";
-//     //     console.log(row);
-//     // }   
+// // // for(let i=1;i<=;i++){
+// //     // let row=""; 
+// //     // for(let j=1;j<=5;j++){
+// //     //     row=row+"* ";
+// //     //     console.log(row);
+// //     // }   
     
     
-// // }
+// // // }
 
-// // for(let i=5;i>=1;i--){
-// //     for(let j=1;j<=i;j++){
-// //         console.log(j);
-// //     }
-// // }
+// // // for(let i=5;i>=1;i--){
+// // //     for(let j=1;j<=i;j++){
+// // //         console.log(j);
+// // //     }
+// // // }
 
-// // let condition=10;
-// // while(condition<=15){
-// //     console.log("while loop",condition);
-// //     condition++;
-// //     // code block to be executed
-// //   }
-// //   console.log("=========================while loop======================");
+// // // // let condition=10;
+// // // // while(condition<=15){
+// // // //     console.log("while loop",condition);
+// // // //     condition++;
+// // // //     // code block to be executed
+// // // //   }
+// // // //   console.log("=========================while loop======================");
   
-// //   let a=0;
-// //   while(a<=5){
-// //     console.log("while loop",a);
-// //     a++;
-// //   }
+// // // //   let a=0;
+// // // //   while(a<=5){
+// // // //     console.log("while loop",a);
+// // // //     a++;
+// // // //   }
 
-// //     console.log("=========================do while loop======================");
-// //     let b=0;a
-// //     do{
-// //         console.log("do while loop",b);
-// //         b++;
-// //     }while(b<=5);
+// // // //     console.log("=========================do while loop======================");
+// // // //     let b=0;a
+// // // //     do{
+// // // //         console.log("do while loop",b);
+// // // //         b++;
+// // // //     }while(b<=5);
 
-// //     console.log("=========================function======================");
-// //    function greet(name){
-// // function rashid(){
-// //         console.log("Hello Rashid");
-// //     }
-// //     rashid();
+// // // //     console.log("=========================function======================");
+// // // //    function greet(name){
+// // // // function rashid(){
+// // // //         console.log("Hello Rashid");
+// // // //     }
+// // // //     rashid();
 
-// // function sum(a,b){
-// //     let total=a+b;
-// //     console.log("Sum:",total);
-// // }
-// // sum(5,10);
-// // function name(){
-// //     console.log("My name is Rashid");
-// // }
-// // name();
+// // // // function sum(a,b){
+// // // //     let total=a+b;
+// // // //     console.log("Sum:",total);
+// // // // }
+// // // // sum(5,10);
+// // // // function name(){
+// // // //     console.log("My name is Rashid");
+// // // // }
+// // // // name();
 
-// //  const num=(...num)=>{
-// //     console.log(num);
+// // // //  const num=(...num)=>{
+// // // //     console.log(num);
     
-// //  }
+// // // //  }
 
-// //  num(1,2,3,4,4,5,6,7,9,0,7,4,2,)
+// // // //  num(1,2,3,4,4,5,6,7,9,0,7,4,2,)
 
-// //  function sum(...numbers) {
-// //   let total = 0;
-// //   for (const num of numbers) {
-// //     // total =total+ num;
-// //     total+=num;
-// //   }
-// //   return total;
-// // }
+// // // //  function sum(...numbers) {
+// // // //   let total = 0;
+// // // //   for (const num of numbers) {
+// // // //     // total =total+ num;
+// // // //     total+=num;
+// // // //   }
+// // // //   return total;
+// // // // }
 
-// // console.log(sum(1, 2, 3, 4));
-// // let q=3
-// // let b=4
+// // // // console.log(sum(1, 2, 3, 4));
+// // // // let q=3
+// // // // let b=4
 
-// let arr=[1,2,3,4,5,6];
-// console.log(arr);
-// console.log(`index 0 value is ${arr[0]}`);
-// console.log(`index 1 value is ${arr[1]}`);
-// console.log(`index 2 value is ${arr[2]}`);
-// console.log(`index 3 value is ${arr[3]}`);
-// console.log(`index 4 value is ${arr[4]}`);
-// console.log(`index 5 value is ${arr[5]}`);
-// console.log(`index 6 value is ${arr[6]}`);
-// arr.push(6);
-// console.log(`index 6 value is ${arr[6]}`);
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// console.log(arr);
-// let array=["Honda","seventy","Prado","havy","rase bike","xyz",["alike","likeee","rise"]]
+// // // let arr=[1,2,3,4,5,6];
+// // // console.log(arr);
+// // // console.log(`index 0 value is ${arr[0]}`);
+// // // console.log(`index 1 value is ${arr[1]}`);
+// // // console.log(`index 2 value is ${arr[2]}`);
+// // // console.log(`index 3 value is ${arr[3]}`);
+// // // console.log(`index 4 value is ${arr[4]}`);
+// // // console.log(`index 5 value is ${arr[5]}`);
+// // // console.log(`index 6 value is ${arr[6]}`);
+// // // arr.push(6);
+// // // console.log(`index 6 value is ${arr[6]}`);
+// // // arr.pop();
+// // // arr.pop();
+// // // arr.pop();
+// // // arr.pop();
+// // // arr.pop();
+// // // console.log(arr);
+// // // let array=["Honda","seventy","Prado","havy","rase bike","xyz",["alike","likeee","rise"]]
 
-// let [honda,seventy,prado,havy,...data]=array;
+// // // let [honda,seventy,prado,havy,...data]=array;
 
-// console.log(honda);
-// console.log(prado);
-// console.log(havy);
-// console.log(data);
+// // // console.log(honda);
+// // // console.log(prado);
+// // // console.log(havy);
+// // // console.log(data);
 
-// let =12412412;
+// // // let =12412412;
 
 
-let obj={
-    name:"rashid ",
-    age:23,
-    address:{
-    city:["kHP", "skardu", "shiger","rundu"],
-    zipCode:16100
-    },
-    mark:()=>{
-        console.log("you are pass");
+// // let obj={
+// //     name:"rashid ",
+// //     age:23,
+// //     address:{
+// //     city:["kHP", "skardu", "shiger","rundu"],
+// //     zipCode:16100
+// //     },
+// //     mark:()=>{
+// //         console.log("you are pass");
         
 
-    },
-    cout:"USA",
+// //     },
+// //     cout:"USA",
     
+// // }
+
+// // // console.log(obj.name);
+// // // console.log(obj.age);
+// // // console.log(obj.address);
+
+// // // console.log(obj.address.city[1]);
+// // // console.log(obj.address.zipCode);
+
+
+// // // let {name,age,address,zipCode}=obj;
+
+// // // console.log(name);
+
+// // // console.log(age);
+// // // console.log(address);
+// // // console.log(address.city);
+// // let {name, age, ...spread}=obj
+// // console.log(name);
+// // console.log(age);
+// // console.log(spread);
+// // let {cout="pk"}=obj
+
+
+// // const person = {
+// //   name: "Alice",
+// //   age: 25,
+// //   location: "New York"
+// // };
+
+// // const { country = "USA" } = person;
+// // console.log(country);
+
+
+// // higher order function
+
+// let fun=()=>{
+//     console.log("this is a function"); 
+//  }
+// // fun();
+
+// function higherOrderFunction(callback){
+//     console.log("this is a higher order function");
+//     callback();
+// }
+// higherOrderFunction(fun);
+
+
+
+// Hoisting
+// a=30
+console.log("a:"+a);
+var a;
+
+
+hi();
+function hi(){
+    console.log("hello");
 }
+// hi2();
+// const hi2=()=>{
+//     console.log("hello2");
+// }
+// var a=32
+// var a=21;
+// let a=34;
+// let a=53;
+// console.log(a);
 
-// console.log(obj.name);
-// console.log(obj.age);
-// console.log(obj.address);
+// let c=32
+// let c=21;
+// console.log(c);
 
-// console.log(obj.address.city[1]);
-// console.log(obj.address.zipCode);
-
-
-// let {name,age,address,zipCode}=obj;
-
-// console.log(name);
-
-// console.log(age);
-// console.log(address);
-// console.log(address.city);
-let {name, age, ...spread}=obj
-console.log(name);
-console.log(age);
-console.log(spread);
-let {cout="pk"}=obj
-
-
-const person = {
-  name: "Alice",
-  age: 25,
-  location: "New York"
-};
-
-const { country = "USA" } = person;
-console.log(country);
-
-
-
-
-
-
-
+function delay(){
+    console.log("fist time");
+}
+setTimeout(delay,3000)
+setTimeout(()=>{
+    console.log("second time");
+},500)
 
 
 
