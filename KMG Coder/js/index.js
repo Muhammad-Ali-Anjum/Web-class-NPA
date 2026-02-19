ced5b3026c46228fa4296ea917211d952339c77b
Muhammad ALi Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-19 15:44:38 +0500

    ad a lect

--- KMG Coder/js/index.js
+++ KMG Coder/js/index.js
@@ -1,111 +1,137 @@
-// // // 
+// // // // 
 
-// // // function
-// // function name(){
-// // console.log("server is runing....");
+// // // // function
+// // // function name(){
+// // // console.log("server is runing....");
 
-// // }
+// // // }
 
-// // name();
+// // // name();
 
-// // function add(a,b){
-// //     console.log(a+b);
+// // // function add(a,b){
+// // //     console.log(a+b);
     
-// // }
-// // add(10,30);
+// // // }
+// // // add(10,30);
 
-// // function sub(a,b){
-// // return a-b;
-// // }
-// // let result=sub(20,10);
-// // console.log(result);
-
-
-// // arrow function
-// // let add=(a,b)=>{
-// //     console.log(a+b);
-// // }
-// // add(10,20);
-// // let hi=()=>{
-// //     console.log("hi");
-// // }
-// // hi();
-// // let syed=()=>{
-// //     console.log("syed");
-// // }
-// // syed();
-// // let syeed=()=> console.log("syeed");
-
-// // syeed();
-// // let add=(a,b)=> {
-// //     return a+b;
-// // }
-// // let result=add(10,20);
-// // console.log(result);
-// // console.log("==================");
-// // let adds=(a,b)=> a+b;
-// // let results=adds(20,30);
-// // console.log(results);
-
-// // const checkAge = (age) => age >= 18 ? 'Adult' : 'Minor';
-
-// // console.log(checkAge(20)); 
-// // console.log(checkAge(15));
-
-// // let votechecker=(age)=>{
-// //     return age>=18? "you are eligible for vote":"you are not eligible for vote";
-// // }
-// // console.log(votechecker(20));
-// // console.log(votechecker(15));
-
-// // let votecheck=(age)=> age>=18? "you are eligible for vote":"you are not eligible for vote";
-// // console.log(votecheck(20));
-// // console.log(votecheck(15));
-// // let votecheck=age=> age>=18? "you are eligible for vote":"you are not eligible for vote";
-// // console.log(votecheck(20));
-// // console.log(votecheck(15));
-
-// let votechecks=age=> {
-//     if(age>=18){
-//         return "you are eligible for vote";
-//     }
-//     return "you are not eligible for vote";
-// };
-// // console.log(votechecks(20));
-// console.log(votechecks(15));
+// // // function sub(a,b){
+// // // return a-b;
+// // // }
+// // // let result=sub(20,10);
+// // // console.log(result);
 
 
-// Advanced Techniques with Higher Order Functions
-function add(x) {
-    return x + 2;
-}
-function mul(x) {
-    return x * 3;
-}
-function compose(f, g) {
-    return function(x) {
-        return f(g(x));
-  }; }
-const addThenMul = compose(mul, add)(4);
-// const result = addThenMul(4);
-console.log(addThenMul); // Output: 21
+// // // arrow function
+// // // let add=(a,b)=>{
+// // //     console.log(a+b);
+// // // }
+// // // add(10,20);
+// // // let hi=()=>{
+// // //     console.log("hi");
+// // // }
+// // // hi();
+// // // let syed=()=>{
+// // //     console.log("syed");
+// // // }
+// // // syed();
+// // // let syeed=()=> console.log("syeed");
 
-function mul(x) {
-    return function(y) {
-        return x * y;
-  };
-}
-var mulFn = mul(2);
-console.log(mulFn(5));
+// // // syeed();
+// // // let add=(a,b)=> {
+// // //     return a+b;
+// // // }
+// // // let result=add(10,20);
+// // // console.log(result);
+// // // console.log("==================");
+// // // let adds=(a,b)=> a+b;
+// // // let results=adds(20,30);
+// // // console.log(results);
 
-function hi(kuchbifun){
-    console.log("first function");
-    kuchbifun()
+// // // const checkAge = (age) => age >= 18 ? 'Adult' : 'Minor';
+
+// // // console.log(checkAge(20)); 
+// // // console.log(checkAge(15));
+
+// // // let votechecker=(age)=>{
+// // //     return age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // // }
+// // // console.log(votechecker(20));
+// // // console.log(votechecker(15));
+
+// // // let votecheck=(age)=> age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // // console.log(votecheck(20));
+// // // console.log(votecheck(15));
+// // // let votecheck=age=> age>=18? "you are eligible for vote":"you are not eligible for vote";
+// // // console.log(votecheck(20));
+// // // console.log(votecheck(15));
+
+// // let votechecks=age=> {
+// //     if(age>=18){
+// //         return "you are eligible for vote";
+// //     }
+// //     return "you are not eligible for vote";
+// // };
+// // // console.log(votechecks(20));
+// // console.log(votechecks(15));
+
+
+// // Advanced Techniques with Higher Order Functions
+// function add(x) {
+//     return x + 2;
+// }
+// function mul(x) {
+//     return x * 3;
+// }
+// function compose(f, g) {
+//     return function(x) {
+//         return f(g(x));
+//   }; }
+// const addThenMul = compose(mul, add)(4);
+// // const result = addThenMul(4);
+// console.log(addThenMul); // Output: 21
+
+// function mul(x) {
+//     return function(y) {
+//         return x * y;
+//   };
+// }
+// var mulFn = mul(2);
+// console.log(mulFn(5));
+
+// function hi(kuchbifun){
+//     console.log("first function");
+//     kuchbifun()
     
-}
-function kuchbifun(){
-    console.log("second function");
-}
+// }
+// function kuchbifun(){
+//     console.log("second function");
+// }
 
 
-hi(kuchbifun);
+// hi(kuchbifun);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(10,20,20,30)); // Output: 30
+function add(a,b,...args){
+    let sum=a+b; 
+    for(let i of args){
+        sum+=i;
+    }
+    return sum;
+}
+
+console.log(add(10,20,20,30,30));
+
+function sum(...numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5)); 
+
+function multiply(...numbers) {
+    return numbers.reduce((a,b)=>a*b,1)
+}
+console.log(multiply(1, 2, 3, 4, 5));
