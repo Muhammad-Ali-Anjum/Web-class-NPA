7dfe1e24bfe76d4214ed14fc397a6c54d85cc062
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-22 15:28:28 +0500

    add a lect

--- Rashid/index.js
+++ Rashid/index.js
@@ -1,273 +1,293 @@
-// // // // logal operter
-// // // // && oper
-// // // // T T T
-// // // // T F F
-// // // // F T F
-// // // // F F F
-// // // // OR || OPR
-// // // // T T T
-// // // // T F T
-// // // // F T T
-// // // // F F F
-// // // // NOT !
-// // // // T F
-// // // // F T
-// // // console.log("setart");
-// // // const total=85+90+80;
-// // // const mark=(total/300)*100;
-// // // console.log("percentage",mark);
+// // // // // logal operter
+// // // // // && oper
+// // // // // T T T
+// // // // // T F F
+// // // // // F T F
+// // // // // F F F
+// // // // // OR || OPR
+// // // // // T T T
+// // // // // T F T
+// // // // // F T T
+// // // // // F F F
+// // // // // NOT !
+// // // // // T F
+// // // // // F T
+// // // // console.log("setart");
+// // // // const total=85+90+80;
+// // // // const mark=(total/300)*100;
+// // // // console.log("percentage",mark);
 
-// // // // let mark=45;
-// // // if(mark>80 && mark<=100){
-// // //     console.log("A");
+// // // // // let mark=45;
+// // // // if(mark>80 && mark<=100){
+// // // //     console.log("A");
     
-// // // }
-// // // else if(mark>=60 && mark<80){
-// // //     console.log("B"); 
-// // //   }
-// // // else if(mark>=40 && mark<60){
-// // //     console.log("C");  
-// // //  }
-// // // else if(mark>=30 && mark<40){
-// // //     console.log("D");  
-// // //  }
-
-// // // else if(mark<0 || mark>100){
-// // //     console.log("invalid");
-// // //   }
-// // //  else{
-// // //         console.log("Fail");        
-// // //     }
-
-
-
-// // // console.log("========================class start===========================");
-
-// // // let opr="+";
-
-// // // switch(opr){
-// // //     case "+":
-// // //         let a=5;
-// // //         let b=10;
-
-// // //         console.log("Addition", a+b);   
-// // //         break;
-// // //     case "-": 
-// // //         console.log("Subtraction");
-// // //         break;
-// // //     case "*":
-// // //         console.log("Multiplication"); 
-// // //         break;
-// // //     default:
-// // //         console.log("invlid operator");
-// // // }
-        
-
-// // // for(let i=1;i<=;i++){
-// //     // let row=""; 
-// //     // for(let j=1;j<=5;j++){
-// //     //     row=row+"* ";
-// //     //     console.log(row);
-// //     // }   
-    
-    
-// // // }
-
-// // // for(let i=5;i>=1;i--){
-// // //     for(let j=1;j<=i;j++){
-// // //         console.log(j);
-// // //     }
-// // // }
-
-// // // // let condition=10;
-// // // // while(condition<=15){
-// // // //     console.log("while loop",condition);
-// // // //     condition++;
-// // // //     // code block to be executed
-// // // //   }
-// // // //   console.log("=========================while loop======================");
-  
-// // // //   let a=0;
-// // // //   while(a<=5){
-// // // //     console.log("while loop",a);
-// // // //     a++;
-// // // //   }
-
-// // // //     console.log("=========================do while loop======================");
-// // // //     let b=0;a
-// // // //     do{
-// // // //         console.log("do while loop",b);
-// // // //         b++;
-// // // //     }while(b<=5);
-
-// // // //     console.log("=========================function======================");
-// // // //    function greet(name){
-// // // // function rashid(){
-// // // //         console.log("Hello Rashid");
-// // // //     }
-// // // //     rashid();
-
-// // // // function sum(a,b){
-// // // //     let total=a+b;
-// // // //     console.log("Sum:",total);
 // // // // }
-// // // // sum(5,10);
-// // // // function name(){
-// // // //     console.log("My name is Rashid");
-// // // // }
-// // // // name();
-
-// // // //  const num=(...num)=>{
-// // // //     console.log(num);
-    
+// // // // else if(mark>=60 && mark<80){
+// // // //     console.log("B"); 
+// // // //   }
+// // // // else if(mark>=40 && mark<60){
+// // // //     console.log("C");  
+// // // //  }
+// // // // else if(mark>=30 && mark<40){
+// // // //     console.log("D");  
 // // // //  }
 
-// // // //  num(1,2,3,4,4,5,6,7,9,0,7,4,2,)
-
-// // // //  function sum(...numbers) {
-// // // //   let total = 0;
-// // // //   for (const num of numbers) {
-// // // //     // total =total+ num;
-// // // //     total+=num;
+// // // // else if(mark<0 || mark>100){
+// // // //     console.log("invalid");
 // // // //   }
-// // // //   return total;
+// // // //  else{
+// // // //         console.log("Fail");        
+// // // //     }
+
+
+
+// // // // console.log("========================class start===========================");
+
+// // // // let opr="+";
+
+// // // // switch(opr){
+// // // //     case "+":
+// // // //         let a=5;
+// // // //         let b=10;
+
+// // // //         console.log("Addition", a+b);   
+// // // //         break;
+// // // //     case "-": 
+// // // //         console.log("Subtraction");
+// // // //         break;
+// // // //     case "*":
+// // // //         console.log("Multiplication"); 
+// // // //         break;
+// // // //     default:
+// // // //         console.log("invlid operator");
 // // // // }
-
-// // // // console.log(sum(1, 2, 3, 4));
-// // // // let q=3
-// // // // let b=4
-
-// // // let arr=[1,2,3,4,5,6];
-// // // console.log(arr);
-// // // console.log(`index 0 value is ${arr[0]}`);
-// // // console.log(`index 1 value is ${arr[1]}`);
-// // // console.log(`index 2 value is ${arr[2]}`);
-// // // console.log(`index 3 value is ${arr[3]}`);
-// // // console.log(`index 4 value is ${arr[4]}`);
-// // // console.log(`index 5 value is ${arr[5]}`);
-// // // console.log(`index 6 value is ${arr[6]}`);
-// // // arr.push(6);
-// // // console.log(`index 6 value is ${arr[6]}`);
-// // // arr.pop();
-// // // arr.pop();
-// // // arr.pop();
-// // // arr.pop();
-// // // arr.pop();
-// // // console.log(arr);
-// // // let array=["Honda","seventy","Prado","havy","rase bike","xyz",["alike","likeee","rise"]]
-
-// // // let [honda,seventy,prado,havy,...data]=array;
-
-// // // console.log(honda);
-// // // console.log(prado);
-// // // console.log(havy);
-// // // console.log(data);
-
-// // // let =12412412;
-
-
-// // let obj={
-// //     name:"rashid ",
-// //     age:23,
-// //     address:{
-// //     city:["kHP", "skardu", "shiger","rundu"],
-// //     zipCode:16100
-// //     },
-// //     mark:()=>{
-// //         console.log("you are pass");
         
 
-// //     },
-// //     cout:"USA",
+// // // // for(let i=1;i<=;i++){
+// // //     // let row=""; 
+// // //     // for(let j=1;j<=5;j++){
+// // //     //     row=row+"* ";
+// // //     //     console.log(row);
+// // //     // }   
     
-// // }
+    
+// // // // }
 
-// // // console.log(obj.name);
-// // // console.log(obj.age);
-// // // console.log(obj.address);
+// // // // for(let i=5;i>=1;i--){
+// // // //     for(let j=1;j<=i;j++){
+// // // //         console.log(j);
+// // // //     }
+// // // // }
 
-// // // console.log(obj.address.city[1]);
-// // // console.log(obj.address.zipCode);
+// // // // // let condition=10;
+// // // // // while(condition<=15){
+// // // // //     console.log("while loop",condition);
+// // // // //     condition++;
+// // // // //     // code block to be executed
+// // // // //   }
+// // // // //   console.log("=========================while loop======================");
+  
+// // // // //   let a=0;
+// // // // //   while(a<=5){
+// // // // //     console.log("while loop",a);
+// // // // //     a++;
+// // // // //   }
+
+// // // // //     console.log("=========================do while loop======================");
+// // // // //     let b=0;a
+// // // // //     do{
+// // // // //         console.log("do while loop",b);
+// // // // //         b++;
+// // // // //     }while(b<=5);
+
+// // // // //     console.log("=========================function======================");
+// // // // //    function greet(name){
+// // // // // function rashid(){
+// // // // //         console.log("Hello Rashid");
+// // // // //     }
+// // // // //     rashid();
+
+// // // // // function sum(a,b){
+// // // // //     let total=a+b;
+// // // // //     console.log("Sum:",total);
+// // // // // }
+// // // // // sum(5,10);
+// // // // // function name(){
+// // // // //     console.log("My name is Rashid");
+// // // // // }
+// // // // // name();
+
+// // // // //  const num=(...num)=>{
+// // // // //     console.log(num);
+    
+// // // // //  }
+
+// // // // //  num(1,2,3,4,4,5,6,7,9,0,7,4,2,)
+
+// // // // //  function sum(...numbers) {
+// // // // //   let total = 0;
+// // // // //   for (const num of numbers) {
+// // // // //     // total =total+ num;
+// // // // //     total+=num;
+// // // // //   }
+// // // // //   return total;
+// // // // // }
+
+// // // // // console.log(sum(1, 2, 3, 4));
+// // // // // let q=3
+// // // // // let b=4
+
+// // // // let arr=[1,2,3,4,5,6];
+// // // // console.log(arr);
+// // // // console.log(`index 0 value is ${arr[0]}`);
+// // // // console.log(`index 1 value is ${arr[1]}`);
+// // // // console.log(`index 2 value is ${arr[2]}`);
+// // // // console.log(`index 3 value is ${arr[3]}`);
+// // // // console.log(`index 4 value is ${arr[4]}`);
+// // // // console.log(`index 5 value is ${arr[5]}`);
+// // // // console.log(`index 6 value is ${arr[6]}`);
+// // // // arr.push(6);
+// // // // console.log(`index 6 value is ${arr[6]}`);
+// // // // arr.pop();
+// // // // arr.pop();
+// // // // arr.pop();
+// // // // arr.pop();
+// // // // arr.pop();
+// // // // console.log(arr);
+// // // // let array=["Honda","seventy","Prado","havy","rase bike","xyz",["alike","likeee","rise"]]
+
+// // // // let [honda,seventy,prado,havy,...data]=array;
+
+// // // // console.log(honda);
+// // // // console.log(prado);
+// // // // console.log(havy);
+// // // // console.log(data);
+
+// // // // let =12412412;
 
 
-// // // let {name,age,address,zipCode}=obj;
+// // // let obj={
+// // //     name:"rashid ",
+// // //     age:23,
+// // //     address:{
+// // //     city:["kHP", "skardu", "shiger","rundu"],
+// // //     zipCode:16100
+// // //     },
+// // //     mark:()=>{
+// // //         console.log("you are pass");
+        
 
+// // //     },
+// // //     cout:"USA",
+    
+// // // }
+
+// // // // console.log(obj.name);
+// // // // console.log(obj.age);
+// // // // console.log(obj.address);
+
+// // // // console.log(obj.address.city[1]);
+// // // // console.log(obj.address.zipCode);
+
+
+// // // // let {name,age,address,zipCode}=obj;
+
+// // // // console.log(name);
+
+// // // // console.log(age);
+// // // // console.log(address);
+// // // // console.log(address.city);
+// // // let {name, age, ...spread}=obj
 // // // console.log(name);
-
 // // // console.log(age);
-// // // console.log(address);
-// // // console.log(address.city);
-// // let {name, age, ...spread}=obj
-// // console.log(name);
-// // console.log(age);
-// // console.log(spread);
-// // let {cout="pk"}=obj
+// // // console.log(spread);
+// // // let {cout="pk"}=obj
 
 
-// // const person = {
-// //   name: "Alice",
-// //   age: 25,
-// //   location: "New York"
-// // };
+// // // const person = {
+// // //   name: "Alice",
+// // //   age: 25,
+// // //   location: "New York"
+// // // };
 
-// // const { country = "USA" } = person;
-// // console.log(country);
+// // // const { country = "USA" } = person;
+// // // console.log(country);
 
 
-// // higher order function
+// // // higher order function
 
-// let fun=()=>{
-//     console.log("this is a function"); 
-//  }
-// // fun();
+// // let fun=()=>{
+// //     console.log("this is a function"); 
+// //  }
+// // // fun();
 
-// function higherOrderFunction(callback){
-//     console.log("this is a higher order function");
-//     callback();
+// // function higherOrderFunction(callback){
+// //     console.log("this is a higher order function");
+// //     callback();
+// // }
+// // higherOrderFunction(fun);
+
+
+
+// // Hoisting
+// // a=30
+// console.log("a:"+a);
+// var a;
+
+
+// hi();
+// function hi(){
+//     console.log("hello");
 // }
-// higherOrderFunction(fun);
+// // hi2();
+// // const hi2=()=>{
+// //     console.log("hello2");
+// // }
+// // var a=32
+// // var a=21;
+// // let a=34;
+// // let a=53;
+// // console.log(a);
 
+// // let c=32
+// // let c=21;
+// // console.log(c);
 
-
-// Hoisting
-// a=30
-console.log("a:"+a);
-var a;
-
-
-hi();
-function hi(){
-    console.log("hello");
-}
-// hi2();
-// const hi2=()=>{
-//     console.log("hello2");
+// function delay(){
+//     console.log("fist time");
 // }
-// var a=32
-// var a=21;
-// let a=34;
-// let a=53;
-// console.log(a);
+// setTimeout(delay,3000)
+// setTimeout(()=>{
+//     console.log("second time");
+// },500)
 
-// let c=32
-// let c=21;
-// console.log(c);
+// console.log("================");
 
-function delay(){
-    console.log("fist time");
-}
-setTimeout(delay,3000)
-setTimeout(()=>{
-    console.log("second time");
-},500)
-
-console.log("================");
-
-let intervalId=setInterval(()=>{
-    console.log("this is set interval");
-},2000)
-console.log("this is after set interval");
-clearInterval(intervalId);
+// let intervalId=setInterval(()=>{
+//     console.log("this is set interval");
+// },2000)
+// console.log("this is after set interval");
+// clearInterval(intervalId);
 
 
 
+
+let checkEven = new Promise((resolve, reject) => {
+    let number = 4;
+    if (number % 2 === 0) resolve("The number is even!");
+    else reject("The number is odd!");
+});
+checkEven
+    .then((message) => console.log(message)) // On success
+    .catch((error) => console.error(error)); // On failure
+
+let prom=new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res("promiss sucess");
+        rej("promiss reject")
+
+    },500)
+})
+prom.then((e)=>console.log(e)
+).catch((rej)=>console.error(rej)
+)
