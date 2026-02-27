28ce90a8b4fc94bc375f6c5c71df326ba3490510
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-27 15:51:57 +0500

    add a lect

--- sara coder/js/index.js
+++ sara coder/js/index.js
@@ -1,7 +1,58 @@
-console.log("hi js");
-console.log("this is first program");
-console.log("nodmon runing...........");
+// // console.log("hi js");
+// // console.log("this is first program");
+// // console.log("nodmon runing...........");
 
-// js
-// 1995
-// es6
+// // // js
+// // // 1995
+// // // es6
+// // data type
+// var a=20;
+// var a=40
+// console.log(a);
+// let b="xyz";
+// {
+//     let b="abc"
+// }
+// console.log(b);
+// const c=32;
+// console.log(c);
+// // oper
+// =
+// arth
+// +-*%
+// <=
+// >=
+// <
+// >
+// !
+// logical
+// &&
+// T T T
+// T F F
+// F T F
+// F F F
+// OR ||
+// T T T
+// T F T
+// F T T
+// F F F
+// NOT
+// !
+// T F
+// F T
+
+let a =43;
+let b=54;
+
+
+let result=b+a;
+console.log("sum of two value",result);
+let re=a-b;
+console.log(`the sub two value ${re}`);
+
+console.log(true*1);
+console.log(true-4);
+
+
+
+
