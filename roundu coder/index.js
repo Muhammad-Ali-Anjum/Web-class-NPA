f1684f67435e860dc9ff1767f5c80645fc739457
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-02-27 16:29:26 +0500

    add a lect

---
+++ roundu coder/index.js
@@ -0,0 +1,41 @@
+// // js 
+// // node js 
+// // nodemon
+// console.log("hi this is my forst program in js");
+
+// var a=32;
+// var a=50;
+// console.log(a);
+// let b=43;
+// {
+ 
+    
+//     let b=4;
+//        console.log(b);
+// }
+// console.log(b);
+
+// const c=43
+// console.log(c);
+// // +_*%/
+// // log
+// & 
+// T T T 
+// T F F
+// F T F
+// F F F
+// ||
+// T T T 
+// T F T 
+// F T T
+// F F F
+
+// NOT !
+// T F
+// F T
+
+
+
+let a=4
+let b=4
+console.log(a-b);
