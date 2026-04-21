2e3784971b8402a0052d7e1aa5790035d2ef64ba
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-04-21 18:07:40 +0500

    add a file

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { Rating } from '@mui/material'
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <h1 className='text-3xl text-blue-400'>
+      This is our home page
+    </h1>
+    <Rating name="size-large" defaultValue={2} size="large" />
+    
+    </>
   )
 }
 
