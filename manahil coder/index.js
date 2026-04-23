aa41c7e77a864b545f3a9dd84a351dffec317537
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-04-23 17:16:32 +0500

    add a home screen

---
+++ manahil coder/index.js
@@ -0,0 +1,85 @@
+// console.log("hi this is my first program");
+// console.log("hi this is my first program");
+// V8 engine is a JavaScript engine developed by Google. It is used in the Chrome browser and Node.js runtime environment. V8 compiles JavaScript code into machine code, which allows for faster execution. It also includes features such as garbage collection and just-in-time (JIT) compilation to optimize performance. V8 is open source and has been widely adopted in the JavaScript community.
+var a = 10;
+console.log(a);
+
+// data type
+// pramative non paramative
+// pramative data type
+// number string boolean null undefined symbol bigInt
+// non pramative data type
+// object array function
+var b = "manahil";
+// console.log(b);
+
+var b="xyz";
+console.log(b);
+
+let c = "xyz";
+console.log(c);
+// let c="abc";
+// console.log(c);
+
+const sm=43
+
+console.log(sm);
+// const sm=45
+// console.log(sm);
+
+// operator
+// arthmetic operator
+// + - * / % **
+var x=10;
+var y=20;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+// logical operator
+// && || !
+var p=true;
+var q=false;
+console.log(p&&q);
+console.log(p||q);
+console.log(!p);
+console.log(!q);
+// T T T
+// T F F
+// F T F
+// F F F
+// or
+// T T T
+// T F T
+// F T T
+// F F F
+// assignment operator
+// = += -= *= /= %=
+var m=10;
+console.log(m);
+m+=5;
+// m=m+5;
+console.log(m);
+m-=3;
+console.log(m);
+m*=2;
+console.log(m);
+m/=4;
+console.log(m);
+m%=3;
+console.log(m);
+
+// comparison operator
+// == === != !== > < >= <=
+var a=10;
+var b=20;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
