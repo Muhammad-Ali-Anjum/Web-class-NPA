5c27cea741e10a3736de18d7e2309c66cbfb3130
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-05-06 17:51:07 +0500

    add a lect

--- taha coder/index.js
+++ taha coder/index.js
@@ -1,4 +1,30 @@
 console.log("HI this is our first project");
 console.log("hi");
 
+// datatype
+// paramative 
+// string
+// number
+// boolean
+// null
+// undefined
+// non paramative
+// array
+// object
+// function
 
+var name = "taha";
+// console.log(name);
+var name="xyz coder";
+console.log(name);
+let name1 = "taha coder";
+console.log(name1);
+{
+let name1 = "xyz coder";
+console.log(name1);
+}
+
+const age = 20;
+console.log(age);
+// const age = 30;
+// console.log(age);
