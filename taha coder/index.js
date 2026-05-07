cfe9d0cc7c849cffa991efe95f153d790b051e63
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-05-07 19:04:46 +0500

    add a lect

--- taha coder/index.js
+++ taha coder/index.js
@@ -1,30 +1,87 @@
-console.log("HI this is our first project");
-console.log("hi");
+// console.log("HI this is our first project");
+// console.log("hi");
 
-// datatype
-// paramative 
-// string
-// number
-// boolean
-// null
-// undefined
-// non paramative
-// array
-// object
-// function
+// // datatype
+// // paramative 
+// // string
+// // number
+// // boolean
+// // null
+// // undefined
+// // non paramative
+// // array
+// // object
+// // function
 
-var name = "taha";
+// var name = "taha";
+// // console.log(name);
+// var name="xyz coder";
 // console.log(name);
-var name="xyz coder";
-console.log(name);
-let name1 = "taha coder";
-console.log(name1);
-{
-let name1 = "xyz coder";
-console.log(name1);
-}
+// let name1 = "taha coder";
+// console.log(name1);
+// {
+// let name1 = "xyz coder";
+// console.log(name1);
+// }
 
-const age = 20;
-console.log(age);
+// const age = 20;
+// console.log(age);
 // const age = 30;
 // console.log(age);
+// oprator
+// + - * / % ++ --
+var a = 10;
+var b = 20;
+var sum = a + b;
+console.log(sum);
+var sub = a - b;
+console.log(sub);
+var mul = a * b;
+console.log(mul);
+var div = a / b;
+console.log(div);
+var mod = a % b;
+console.log(mod);
+
+// assign oper
+// = += -= *= /= %=
+var c = 10;
+// c=c+5;
+c+=5;
+console.log(c);
+// c += 5;
+// console.log(c);
+c -= 5;
+console.log(c);
+c *= 5;
+console.log(c);
+c /= 5;
+console.log(c);
+c %= 5;
+console.log(c);
+
+// logic oprator
+// && || !
+// T T T
+// T F F
+// F T F
+// F F F
+// OR OPERATOR
+// T T T
+// T F T
+// F T T
+// F F F
+// NOT OPERATOR
+// !T F
+// !F T
+// CONATIONAL STATEMENT
+// if else
+var age = 20;
+// if(condation){
+//     balck of code 
+// }
+if (age >= 18) {
+    console.log("you are eligible for vote");
+} else {
+    console.log("you are not eligible for vote");
+}
