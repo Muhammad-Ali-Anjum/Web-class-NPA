ea9a08020f1b128d6b4c712223862ded2fceca76
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-06-03 17:34:16 +0500

    updated website heaader

--- my-react-class-app/src/App.jsx
+++ my-react-class-app/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
 function App() {
   return (
-    <div>App</div>
+    <div className=''>
+      <h1 className='text-blue-500'>Hello, React!</h1>
+      <p className='text-gray-600 text-justify ps-9 pr-9 leading-relaxed '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur debitis amet, quidem eius, sunt quam voluptatum iste veniam vero accusamus commodi hic reiciendis magnam fuga a perspiciatis, eligendi odio sint. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem beatae sed dolor, repudiandae excepturi placeat numquam, ad rem impedit autem nesciunt similique. Voluptatem quisquam eos dolore dolor aliquid, nihil veritatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aspernatur voluptatum repudiandae minima quod possimus veniam, necessitatibus perferendis dicta eius corrupti nesciunt. Amet veritatis sunt voluptatibus earum corrupti, commodi laborum.</p>
+      <button className='bg-purple-400 text-white py-2 px-4 rounded-md hover:bg-purple-700 hover:text-gray-300 cursor-pointer ms-10 px-10'>Click Me</button>
+    </div>
   )
 }
-
 export default App
