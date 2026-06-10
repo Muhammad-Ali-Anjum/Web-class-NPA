8b1cc92f04a085d8e516504790f610cbdb78c8ef
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-06-10 16:36:23 +0500

    add a lect

--- manahil coder/promise.js
+++ manahil coder/promise.js
@@ -30,7 +30,7 @@ getUser.then((message) => {
     console.log('this is in the then ' + message);
 }).catch((message) => {
     // console.log('this is in the catch ' + message);
-    
+
     console.error('this is an error ' + message);
 });
 
@@ -46,6 +46,50 @@ function login(username, password) {
 }
 
 login('manahil', '12345').then((message) => {
+    console.log('this is in the then ' + message);
+}).catch((message) => {
+    console.error('this is an error ' + message);
+});
+
+// object
+let user = {
+    username: 'manahil',
+    password: '12345'
+};
+function loginUser(user) {
+    return new Promise((resolve, reject) => {
+        if (user.username === 'manahil' && user.password === '12345') {
+            resolve('login successful');
+        }
+        else {
+            reject('login failed');
+        }
+    });
+}
+
+loginUser(user).then((message) => {
+    console.log('this is in the then ' + message);
+}).catch((message) => {
+    console.error('this is an error ' + message);
+});
+
+// object like realbase data for username and password
+let database = {
+    manahil: '12345',
+    ali: '54321'
+};
+function loginDatabase(username, password) {
+    return new Promise((resolve, reject) => {
+        if (database[username] && database[username] === password) {
+            resolve('login successful');
+        }
+        else {
+            reject('login failed');
+        }
+    });
+}
+
+loginDatabase('manahil', '12345').then((message) => {
     console.log('this is in the then ' + message);
 }).catch((message) => {
     console.error('this is an error ' + message);
