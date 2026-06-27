e44f445a416f34a1beb6b7bf438421d7ac54640c
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-06-27 09:59:13 +0500

    add a array concept in js

---
+++ class 10/arr.js
@@ -0,0 +1,27 @@
+console.log("the app is runing");
+let arr =[1,2,3,4,5,6,7,8,9];
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+arr.push(10);
+arr.push(11);
+arr.push(12);
+console.log(arr);
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+console.log(arr);
+arr.unshift(0)
+arr.unshift(-1);
+arr.unshift(12);
+
+console.log(arr);
+arr.shift()
+arr.shift()
+arr.shift()
+arr.shift()
+console.log(arr);
+
