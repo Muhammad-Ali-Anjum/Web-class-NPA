4919177724913eb8a429ddc840dd0c94937ee486
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-07-01 08:06:22 +0500

    add a array concept in js

---
+++ class 10/array.js
@@ -0,0 +1,37 @@
+console.log("hi this App is runing");
+let arr=[1,2,3,4,5,67,7,5,3,67,45];
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[2]);
+arr.push(12)
+arr.push(0)
+arr.push(20)
+console.log(arr);
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+arr.pop();
+console.log(arr);
+arr.unshift(0);
+arr.unshift(-1);
+arr.unshift(-2);
+arr.unshift(-3);
+
+console.log(arr);
+arr.shift()
+arr.shift()
+arr.shift()
+arr.shift()
+arr.shift()
+arr.shift()
+console.log(arr);
+let sort=[1,2,3,4]
+console.log(sort);
+sort.reverse()
+console.log(sort);
+
+
