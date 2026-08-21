b7380a600338c44bd0dfdcbff0f24d5b167b4a7d
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-08-21 21:54:26 +0500

    add a lect

---
+++ minsa coder/js/index.js
@@ -0,0 +1,24 @@
+console.log("hello World");
+// data type 
+// 1 paramative 
+// its value cannot change / static
+// int , fload , var , let , const , boolean
+// 2 non paraative
+//  can change the value / dynamic
+// array , function ,object
+// paramative data types
+// let var const
+var name="minsa";
+// console.log(name);
+var name="mina";
+// console.log(name);
+let names="minsa";
+console.log(names);
+// let names="mina";
+console.log(names);
+const age=12;
+console.log(age);
+
+
+
+
