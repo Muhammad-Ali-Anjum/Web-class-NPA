89389c4be51431d62018d4f8c8e46a40ba679782
Muhammad Ali Anjum <muhammadalianjumbalghari@gmail.com>
2026-09-16 18:35:19 +0500

    add a lect

--- mina/fundamental.js
+++ mina/fundamental.js
@@ -1,4 +1,30 @@
 // node js 
 //  nodemon
-console.log("hello world");
-console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+// data types
+// two type data type
+// paramative datatype
+// Non paramative datatypes
+// paramaive data types 
+// it cannot change the data 
+// varibale and costant
+// varibale : it can be change value 
+// contast : value cannot chageable
+//  paramative : paramative is a ds datatype which cannot change the value
+// int var let const flooat , boolean
+// operator 
+//  assign operator  it represt by =
+var a=12;
+var a=43;
+console.log(a);
+let b=22;
+console.log(b);
+// let b=34;
+const c=21;
+console.log(c);
+const c=43
+
+
+
